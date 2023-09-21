417d71da145aaa83286aa271656f0bed2dc30592
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-09-21 16:37:35 +0300

    setting up webpack Encore

--- src/Controller/SongApiController.php
+++ src/Controller/SongApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,13 +13,17 @@ class SongApiController extends AbstractController
 {
 
     #[Route('api/song/{id<\d+>}', methods: ['GET'])]
-    public function songAPI($id): Response
+    public function songAPI(int $id, LoggerInterface $logger): Response
     {
         $song = [
             'id' => $id,
             'name' => 'Waterfalls',
             'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
         ];
+
+        $logger->info("returning {song} from the logger", [
+            'song'=> $id
+        ]);
         return new JsonResponse($song);
     }
 
