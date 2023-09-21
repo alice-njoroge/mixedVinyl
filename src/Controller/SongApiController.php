e37ff0d1cc3ab5917c940d383b627060965822f7
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-09-21 15:20:48 +0300

    returning a JSON object

---
+++ src/Controller/SongApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SongApiController extends AbstractController
+{
+
+    #[Route('api/song/{id}')]
+    public function songAPI($id): Response
+    {
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+        return new JsonResponse($song);
+    }
+
+}
